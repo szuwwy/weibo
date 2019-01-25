62c748e5d62ba3f4d7009c1a15ae1d16488354ee
wwy <120046911@qq.com>
2019-01-25 16:38:25 +0800

    发布微博

---
+++ app/Http/Controllers/StatusesController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Auth;
+
+class StatusesController extends Controller
+{
+    public function __construct()
+    {
+        //首选 except 方法，这样的话，当你新增一个控制器方法时，默认是安全的，此为最佳实践。Laravel 提供的 Auth 中间件在过滤指定动作时，如该用户未通过身份验证（未登录用户），默认将会被重定向到 login 登录页面。
+        $this->middleware('auth');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'content' => 'required|max:140'
+        ]);
+
+        Auth::user()->statuses()->create([
+            'content' => $request->content
+        ]);
+
+        session()->flash('success', '发布成功！');
+
+        return redirect()->back();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -49,4 +49,7 @@ Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm
 Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
 
 
+Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
+
+
 
