82284236896fb1dfbb7d3848ca0749a2212c66d4
wwy <120046911@qq.com>
2019-01-25 14:51:05 +0800

    移除邮件发送的 from 选项

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -100,14 +100,14 @@ class UsersController extends Controller
     {
         $view    = 'emails.confirm';
         $data    = compact('user');
-        $from    = 'summer@example.com';
-        $name    = 'Summer';
+        // $from    = 'summer@example.com';
+        // $name    = 'Summer';
         $to      = $user->email;
         $subject = "感谢注册 Weibo 应用！请确认你的邮箱。";
 
-        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
+        Mail::send($view, $data, function ($message) use ($to, $subject) {
             //$message 邮件消息实例
-            $message->from($from, $name)->to($to)->subject($subject);
+            $message->to($to)->subject($subject);
         });
     }
 
